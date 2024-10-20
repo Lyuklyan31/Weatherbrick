35e53a04721651859646c0d851d66b1c8baf7c70
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-20 15:14:28 +0300

    Added to MainViewController locationButton, backroundView, viewModel.

--- WeatherForecastBrick/WeatherForecastBrick/MainViewController.swift
+++ WeatherForecastBrick/WeatherForecastBrick/MainViewController.swift
@@ -1,22 +1,46 @@
-//
-//  ViewController.swift
-//  WeatherForecastBrick
-//
-//  Created by Mac on 17.10.2024.
-//
-
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
-
+    let viewModel = MainViewModel()
+    
+    let backgroundView = BackgroundView()
+    let locationButton = UIButton()
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        let gradientView = GradientView(frame: view.bounds)
-        view.addSubview(gradientView)
+        setupBackroundView()
+        setupLocationButton()
     }
 
+    func setupBackroundView() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(backgroundView)
+        
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
     
-
+    func setupLocationButton() {
+        locationButton.setTitle("Open Sheet", for: .normal)
+        locationButton.setTitleColor(.black, for: .normal)
+        locationButton.addTarget(self, action: #selector(openSheet), for: .touchUpInside)
+        
+        view.addSubview(locationButton)
+        
+        locationButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    @objc func openSheet() {
+        let sheetViewController = SheetViewController()
+        sheetViewController.modalPresentationStyle = .pageSheet
+        
+        self.present(sheetViewController, animated: true, completion: nil)
+    }
 }
 
