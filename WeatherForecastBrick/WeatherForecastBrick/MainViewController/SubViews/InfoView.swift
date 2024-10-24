74eb0e3da543d7f9541eb62e6474f7ea8912bf88
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-24 17:16:03 +0300

    Fix bugs.

--- WeatherForecastBrick/WeatherForecastBrick/MainViewController/SubViews/InfoView.swift
+++ WeatherForecastBrick/WeatherForecastBrick/MainViewController/SubViews/InfoView.swift
@@ -1,10 +1,3 @@
-//
-//  RectangleView.swift
-//  WeatherForecastBrick
-//
-//  Created by Mac on 23.10.2024.
-//
-
 import UIKit
 import SnapKit
 
@@ -23,29 +16,30 @@ class InfoView: UIView {
     private var rainingLabel = UILabel()
     private var sunnyLabel = UILabel()
     private var fogLabel = UILabel()
-    private var veryHotLable = UILabel()
-    private var snowLable = UILabel()
+    private var veryHotLabel = UILabel()
+    private var snowLabel = UILabel()
     private var windyLabel = UILabel()
     private var noInternetLabel = UILabel()
     
     var hideAction: (() -> Void)?
     
-    
+    // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupRectangles()
+        setupViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupRectangles() {
+    // MARK: - Setup Views
+    private func setupViews() {
         
+        // MARK: - Setup Down Rectangle View
         downRectangleView.layer.cornerRadius = 10
         downRectangleView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         downRectangleView.backgroundColor = .downRectangleOrange
-        
         addSubview(downRectangleView)
         
         downRectangleView.snp.makeConstraints {
@@ -54,10 +48,10 @@ class InfoView: UIView {
             $0.edges.equalToSuperview()
         }
         
+        // MARK: - Setup Upper Rectangle View
         upperRectangleView.layer.cornerRadius = 10
         upperRectangleView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         upperRectangleView.backgroundColor = .upperRectangleOrange
-        
         downRectangleView.addSubview(upperRectangleView)
         
         upperRectangleView.snp.makeConstraints {
@@ -67,31 +61,43 @@ class InfoView: UIView {
             $0.height.equalTo(372)
         }
         
+        // MARK: - Setup Info Label
         infoLabel.text = "INFO"
         infoLabel.font = UIFont(name: "Ubuntu-Bold", size: 18)
-        
         upperRectangleView.addSubview(infoLabel)
         
         infoLabel.snp.makeConstraints {
+            $0.height.equalTo(22)
             $0.top.equalToSuperview().offset(24)
-            $0.centerX.equalToSuperview()
+            $0.left.equalToSuperview().offset(116)
+            $0.right.equalToSuperview().offset(-109)
         }
         
+        // MARK: - Setup Stack View and Weather Labels
         upperRectangleView.addSubview(stackView)
-        
         stackView.axis = .vertical
         stackView.spacing = 10
         
         rainingLabel.text = "Brick is wet - raining"
-        sunnyLabel.text = "Brick is dry - sunny "
+        sunnyLabel.text = "Brick is dry - sunny"
         fogLabel.text = "Brick is hard to see - fog"
-        veryHotLable.text = "Brick with cracks - very hot "
-        snowLable.text = "Brick with snow - snow"
-        windyLabel.text = "Brick is swinging- windy"
-        noInternetLabel.text = "Brick is gone - No Internet "
+        veryHotLabel.text = "Brick with cracks - very hot"
+        snowLabel.text = "Brick with snow - snow"
+        windyLabel.text = "Brick is swinging - windy"
+        noInternetLabel.text = "Brick is gone - No Internet"
         
-        [rainingLabel, sunnyLabel, fogLabel, veryHotLable, snowLable, windyLabel, noInternetLabel].forEach {
-            $0.font = UIFont(name: "Ubuntu-Regular", size: 17)
+        let labels = [
+            rainingLabel,
+            sunnyLabel,
+            fogLabel,
+            veryHotLabel,
+            snowLabel,
+            windyLabel,
+            noInternetLabel
+        ]
+        
+        labels.forEach {
+            $0.font = UIFont(name: "Ubuntu-Regular", size: 15)
             stackView.addArrangedSubview($0)
         }
         
@@ -100,25 +106,26 @@ class InfoView: UIView {
             $0.horizontalEdges.equalToSuperview().inset(30)
         }
         
+        // MARK: - Setup Background Button View
         backgroundButtonView.layer.cornerCurve = .continuous
         backgroundButtonView.layer.cornerRadius = 15.5
         backgroundButtonView.layer.borderWidth = 1
         backgroundButtonView.layer.borderColor = UIColor.black.cgColor
-        
         upperRectangleView.addSubview(backgroundButtonView)
         
         backgroundButtonView.snp.makeConstraints {
-            $0.top.equalTo(stackView.snp.bottom).offset(40)
+            $0.top.greaterThanOrEqualTo(stackView.snp.bottom)
             $0.left.equalToSuperview().offset(81)
             $0.height.equalTo(31)
             $0.right.equalToSuperview().offset(-73)
             $0.bottom.equalToSuperview().offset(-24)
         }
         
+        // MARK: - Setup Hide Button and Label
         hideLabel.text = "Hide"
         hideLabel.font = UIFont(name: "Ubuntu-Medium", size: 15)
-        
         backgroundButtonView.addSubview(hideLabel)
+        
         hideLabel.snp.makeConstraints {
             $0.centerX.centerY.equalToSuperview()
         }
@@ -126,12 +133,12 @@ class InfoView: UIView {
         backgroundButtonView.addSubview(hideButton)
         hideButton.addTarget(self, action: #selector(hideButtonTapped), for: .touchUpInside)
         
-        
         hideButton.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
     
+    // MARK: - Button Action
     @objc private func hideButtonTapped() {
         hideAction?()
     }
