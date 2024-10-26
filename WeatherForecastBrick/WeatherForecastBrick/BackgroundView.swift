d2b730cf45f5f8cd7a565307390bc8f338d0bb8c
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-26 18:40:37 +0300

    Fix appearance BackgroundView.

--- WeatherForecastBrick/WeatherForecastBrick/BackgroundView.swift
+++ WeatherForecastBrick/WeatherForecastBrick/BackgroundView.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class BackgroundView: UIView {
+    // MARK: - Initialization
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupGradient()
@@ -10,22 +12,26 @@ class BackgroundView: UIView {
         super.init(coder: coder)
     }
     
+    // MARK: - Setup Gradient
+    
     private func setupGradient() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.bounds
-       
+        
         gradientLayer.colors = [
             UIColor.lineaGradientYellow.withAlphaComponent(0.30).cgColor,
             UIColor.lineaGradientBlue.withAlphaComponent(0.30).cgColor
         ]
-       
+        
         gradientLayer.locations = [0.4, 0.6]
         
         self.layer.addSublayer(gradientLayer)
     }
     
-       override func layoutSubviews() {
-           super.layoutSubviews()
+    // MARK: - Layout
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         self.layer.sublayers?.first?.frame = self.bounds
     }
 }
