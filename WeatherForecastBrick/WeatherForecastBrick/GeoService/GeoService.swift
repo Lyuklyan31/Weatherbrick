cdfbadb0d123f44f0d5d6a3f53d718248b2fd869
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-31 16:40:27 +0200

    Fix GeoService.

--- WeatherForecastBrick/WeatherForecastBrick/GeoService/GeoService.swift
+++ WeatherForecastBrick/WeatherForecastBrick/GeoService/GeoService.swift
@@ -3,9 +3,9 @@ import Foundation
 // MARK: - GeoService
 class GeoService {
     
-    // MARK: - Fetch Cities With Query
-    func fetchCities(for query: String) async throws -> [CityData] {
-        let apiKey = getAPIKey()
+    // MARK: - Fetch Cities by Name
+    func fetchCitiesByName(for query: String) async throws -> [CityData] {
+        let apiKey = APIKeyProvider.getAPIKey()
         
         guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
             throw URLError(.badURL)
@@ -16,52 +16,30 @@ class GeoService {
         guard let url = URL(string: urlString) else {
             throw URLError(.badURL)
         }
-            // do catch ввипрвити
-        do {
-            let (data, response) = try await URLSession.shared.data(from: url)
-            
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                throw URLError(.badServerResponse)
-            }
-            
-            let decodedResponse = try JSONDecoder().decode([CityData].self, from: data)
-            return decodedResponse
-            
-        } catch {
-            throw error
-        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let decodedResponse = try JSONDecoder().decode([CityData].self, from: data)
+        return decodedResponse
     }
     
-    // MARK: - Fetch City By Coordinate
-    func fetchCityByCoordinates(lon: Double, lat: Double) async throws -> CityLocation {
-        let apiKey = getAPIKey()
+    // MARK: - Fetch Single City by Coordinates
+    func fetchCityByCoordinates(longitude: Double, latitude: Double) async throws -> CityData {
+        let apiKey = APIKeyProvider.getAPIKey()
         
-        let urlString = "https://api.openweathermap.org/geo/1.0/reverse?lat=\(lat)&lon=\(lon)&limit=1&appid=\(apiKey)"
+        let urlString = "https://api.openweathermap.org/geo/1.0/reverse?lat=\(latitude)&lon=\(longitude)&limit=1&appid=\(apiKey)"
         
         guard let url = URL(string: urlString) else {
             throw URLError(.badURL)
         }
         
-        do {
-            let (data, response) = try await URLSession.shared.data(from: url)
-            
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                throw URLError(.badServerResponse)
-            }
-            
-            let decodedResponse = try JSONDecoder().decode([CityLocation].self, from: data)
-            
-            guard let city = decodedResponse.first else {
-                throw NSError(domain: "No cities found", code: 404, userInfo: nil)
-            }
-            return city
-        } catch {
-            throw error
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let decodedResponse = try JSONDecoder().decode([CityData].self, from: data)
+        
+        guard let city = decodedResponse.first else {
+            throw NSError(domain: "No cities found", code: 404, userInfo: nil)
         }
+        return city
     }
-}
-
-
-private func getAPIKey() -> String {
-    return Bundle.main.object(forInfoDictionaryKey: "WeatherAPIKey") as? String ?? ""
 }
