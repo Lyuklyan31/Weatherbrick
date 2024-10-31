3e54ad65179d806260c2b91b43794fd505a6a000
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-31 16:42:42 +0200

    Fix  Weather Service.

--- WeatherForecastBrick/WeatherForecastBrick/WeatherService/WeatherService.swift
+++ WeatherForecastBrick/WeatherForecastBrick/WeatherService/WeatherService.swift
@@ -6,62 +6,19 @@ class WeatherService {
     // MARK: - Fetch Data
     
     func fetchWeatherData(lat: Double, lon: Double) async throws -> WeatherData {
-        let apiKey = getAPIKey()
+        let apiKey = APIKeyProvider.getAPIKey()
         
         let urlString =  "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)"
      
-        // Validating the URL
         guard let url = URL(string: urlString) else {
             throw URLError(.badURL)
         }
         
-        // Fetching data from the API
         let (data, _) = try await URLSession.shared.data(from: url)
+        let weatherData = try JSONDecoder().decode(WeatherData.self, from: data)
         
-        do {
-            // Decoding the JSON response
-            let weatherData = try JSONDecoder().decode(WeatherData.self, from: data)
-            return weatherData
-        } catch {
-            // Handling decoding errors
-            throw error
-        }
+        return weatherData
     }
 }
 
-//completion handler // основна відмінність більший контроль результатів та підтримка всіх версій IOS
-class WeatherServiceCompletion {
-    func fetchWeatherData(lat: Double, lon: Double, completion: @escaping (Result<WeatherData, Error>) -> Void) {
-        let apiKey = getAPIKey()
-        
-        let urlString = "https://api.openweathermap.org/data/3.0/onecall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(apiKey)&units=metric"
-        
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            
-            do {
-                let weaterData = try JSONDecoder().decode(WeatherData.self, from: data)
-                completion(.success(weaterData))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-        
-        task.resume()
-    }
-}
 
-private func getAPIKey() -> String {
-    return Bundle.main.object(forInfoDictionaryKey: "WeatherAPIKey") as? String ?? ""
-}
