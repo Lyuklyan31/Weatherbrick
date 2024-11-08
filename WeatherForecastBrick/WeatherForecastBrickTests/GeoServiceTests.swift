d7efa9576830c613ee03ede0601e998c4035f5ae
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-08 17:30:05 +0200

    Fix GeoService.

--- WeatherForecastBrick/WeatherForecastBrickTests/GeoServiceTests.swift
+++ WeatherForecastBrick/WeatherForecastBrickTests/GeoServiceTests.swift
@@ -1,29 +1,28 @@
 import XCTest
+import CoreLocation
 @testable import WeatherForecastBrick
 
-// GeoServiceTests
 final class GeoServiceTests: XCTestCase {
-    var geoService: GeoService!
+    static var geoService: GeoService!
     
-    // MARK: - Setup and Teardown
-    
-    override func setUp() {
+    private let kyivCoordinate = CLLocationCoordinate2D(latitude: 50.4501, longitude: 30.5234)
+    private let londonCoordinate = CLLocationCoordinate2D(latitude: 51.5074, longitude: 0.1278)
+        
+    override class func setUp() {
         super.setUp()
         geoService = GeoService()
     }
     
-    override func tearDown() {
+    override class func tearDown() {
         geoService = nil
         super.tearDown()
     }
-    
-    // MARK: - Fetch City Tests
     
     func testFetchCitiesReturnsValidData() async {
         let query = "Kyiv"
         
         do {
-            let cities = try await geoService.fetchCitiesByName(for: query)
+            let cities = try await GeoServiceTests.geoService.fetchCitiesByName(for: query)
             
             XCTAssertGreaterThan(cities.count, 0, "Expected to fetch at least one city.")
             XCTAssertEqual(cities.first?.name, "Kyiv", "Expected the first city to be Kyiv.")
@@ -34,24 +33,21 @@ final class GeoServiceTests: XCTestCase {
     }
     
     func testFetchCitiesReturnsEmptyArray() async {
-        let geoService = GeoService()
+        let query = "nonexistent city"
         
         do {
-            let cities = try await geoService.fetchCitiesByName(for: "nonexistent city")
+            let cities = try await GeoServiceTests.geoService.fetchCitiesByName(for: query)
             XCTAssertTrue(cities.isEmpty, "Expected an empty array when the city does not exist.")
         } catch {
             XCTFail("Unexpected error thrown: \(error)")
         }
     }
     
-    // MARK: Fetch City By Coordinate
-    
     func testFetchByCoordinatesKyiv() async {
-        let longitude = 30.5234
-        let latitude = 50.4501
-        
         do {
-            let city = try await geoService.fetchCityByCoordinates(longitude: longitude, latitude: latitude)
+            let city = try await GeoServiceTests.geoService.fetchCityByCoordinates(
+                longitude: kyivCoordinate.longitude, latitude: kyivCoordinate.latitude
+            )
             
             XCTAssertEqual(city.name, "Kyiv", "Expected the city to be Kyiv.")
             XCTAssertEqual(city.country, "UA", "Expected the city to be Kyiv.")
@@ -61,11 +57,10 @@ final class GeoServiceTests: XCTestCase {
     }
     
     func testFetchByCoordinatesLondon() async {
-        let longitude = 0.1278
-        let latitude = 51.5074
-        
         do {
-            let city = try await geoService.fetchCityByCoordinates(longitude: longitude, latitude: latitude)
+            let city = try await GeoServiceTests.geoService.fetchCityByCoordinates(
+                longitude: londonCoordinate.longitude, latitude: londonCoordinate.latitude
+            )
             
             XCTAssertEqual(city.name, "London", "Expected the city to be London.")
             XCTAssertEqual(city.country, "GB", "Expected the city to be UK.")
